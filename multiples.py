35916f8ec29c9608ab04424b81fe1a6e95b6357f
Elppoliini <elisa.urjansson@hotmail.com>
2024-11-06 13:27:09 +0200

    Add comments

--- multiples.py
+++ multiples.py
@@ -1,6 +1,7 @@
 import re
 import sys
 
+#functionality for counting the multiples of the given two numbers and the goal number
 def multiple (num1, num2, goalnum):
     matchingnumbers = []
     for num in range(1, goalnum):
@@ -11,20 +12,24 @@ def multiple (num1, num2, goalnum):
 def main(inputfile, outputfile):
     infile = open(inputfile, "r")
     rows = []
+    #read all rows of input file and perform multiple() to ones with correct input
     for line in infile:
         splittedline = line.strip().split(" ")
         numlen= len(splittedline)
         if re.match("^[0-9 ]+$", line) and numlen == 3:
             multiples = multiple(int(splittedline[0]), int(splittedline[1]), int(splittedline[2]))
             goal = splittedline[2]
+            #Save values for later
             rows.append((goal, multiples))
     infile.close()
+    #sort rows by ascending order of how many multiples certain row has
     rows.sort(key=lambda row: len(row[1]))
     outfile = open(outputfile, "w")
+    #add rows to output file with wanted format
     for row in rows:
         line = row[0] + ":" + " ".join(row[1]) + "\n"
         outfile.write(line)
         print(line, end="")
     outfile.close()
-    
+
 main(sys.argv[1], sys.argv[2])
